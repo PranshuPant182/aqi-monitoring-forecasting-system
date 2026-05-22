ca009431b9cb0d0edb507128a96107c7e1e5fcc4
PranshuPant182 <gamingm425@gmail.com>
2026-05-22 16:42:38 +0530

    done

--- Backend/db.js
+++ Backend/db.js
@@ -1,6 +1,6 @@
 const sqlite3 = require('sqlite3').verbose();
 
-const db = new sqlite3.Database('../database/air_quality.db', (err) => {
+const db = new sqlite3.Database('../Database/air_quality.db', (err) => {
     if (err) {
         console.error("❌ Database connection failed:", err.message);
     } else {
